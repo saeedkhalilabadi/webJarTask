5b67ae2db1fb7fd3738658f656ca937f6e04c18f
saeedkhalilabadi <saeedkhalilabadi@gmail.com>
2022-12-21 11:42:23 +0330

    add posts data

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains:['placeimg.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'placeimg.com/640/480/technics',
+      },
+      {
+        protocol: 'http',
+        hostname: 'placeimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.example.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
